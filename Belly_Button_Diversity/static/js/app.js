527ca62191a2f7465ba50985c1c52b0f902e4e17
nowlansavage <nowlansavage@gmail.com>
2020-09-30 22:36:00 -0500

    Bar Chart

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -5,7 +5,7 @@ d3.json(file_path).then(function(data){
 	console.log(data);
 	dropDown(data);
 });
-
+//populate the dropdown menu
 function dropDown(sampleData){
 	sampleData['names'].forEach(name=>{
 		var newItem = d3.select('#selDataset').append('option');
@@ -15,30 +15,42 @@ function dropDown(sampleData){
 };
 
 // set the data of the newly selected item
-function newSelection(selected){
+function optionChanged(selected){
 	makeBar(selected);
 	makeTable(selected);
 	makeBubbleMap(selected);
 };
 
+// create bar chart from selected data
 function makeBar(sample){
+	//console.log(sample);
 	d3.json(file_path).then(function(data){
-		var samples =data['samples']
-		var selectedSamples =samples.filter(sample=>sample['id'])
-
+		var samples =data['samples'];
+		var selectedSamples =samples.filter(bug=>bug['id'] ==sample);
+		var currentSample =selectedSamples[0]
+		console.log(currentSample);
+		
 		var traceBar={
-			X: selectedSamples['sample_values'].slice(0,10)
-			y: selectedSamples['otu_ids'].map(otu_ids=>'OTU ' otu_ids)
-			type: 'bar'
-			text: selectedSamples['otu_labels'].slice(0,10)
+			x: currentSample['sample_values'].slice(0,10),
+			y: currentSample['otu_ids'].map(otu_id=>'OTU ' +otu_id).slice(0,10),
+			type: 'bar',
+			text: currentSample['otu_labels'].slice(0,10),
+			orientation: 'h'
 		};
+		var data =[traceBar];
 		var layout ={
-			
-		}
+			title: "Abundance of Navel Microbes",
+			xaxis: { title: "Abundance" },
+			yaxis: { title: "OTU ID"}
+		};
+		Plotly.newPlot('bar', data, layout);
 	});
-
 };
 
 function makeTable(sample){
+
+};
+
+function makeBubbleMap(sample){
 
 };
